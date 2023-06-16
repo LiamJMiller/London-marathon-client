1ff05435eaac725f20adaf2c1bc252383f8ad15e
LiamJMiller <liamjmiller.15@gmail.com>
2023-06-16 12:48:04 +0100

    header styled

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -4,19 +4,25 @@ const DashBoard = () => {
   return (
     <section className="dashboard">
       <div className="dashboard__card">
-        <h3>number</h3>
-        <p>registered</p>
-        <p>spots left</p>
+        <h3 className="dashboard__number">50</h3>
+        <p className="dashboard__subtext dashboard__subtext--main">
+          Registered Athletes
+        </p>
+        <p className="dashboard__subtext">0 spots open</p>
       </div>
       <div className="dashboard__card">
-        <h3>number</h3>
-        <p>registered</p>
-        <p>spots left</p>
+        <h3 className="dashboard__number">10</h3>
+        <p className="dashboard__subtext dashboard__subtext--main">
+          Waitlisted Athletes
+        </p>
+        <p className="dashboard__subtext"></p>
       </div>
-      <div className="dashboard__card">
-        <h3>number</h3>
-        <p>registered</p>
-        <p>spots left</p>
+      <div className="dashboard__card dashboard__card--2">
+        <h3 className="dashboard__number">£500</h3>
+        <p className="dashboard__subtext dashboard__subtext--main">
+          Raised towards PE / IT equipment
+        </p>
+        <p className="dashboard__subtext">Great job! You’ve reached the max</p>
       </div>
     </section>
   );
