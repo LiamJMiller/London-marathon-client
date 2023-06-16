67bd345c4fab869af00a6f63e15960761f948a95
LiamJMiller <liamjmiller.15@gmail.com>
2023-06-16 11:51:15 +0100

    intital table features

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,14 +1,15 @@
+import "./Table.scss";
 import TableFields from "../TableFields/TableFields";
 import TableHeader from "../TableHeader/TableHeader";
 import TableRow from "../TableRow/TableRow";
 
 function Table({ athletes }) {
   return (
-    <>
+    <section className="table">
       <TableHeader />
       <TableFields />
       <TableRow /> {/* Map athletes to TableRows */}
-    </>
+    </section>
   );
 }
 
--- src/components/TableHeader/TableHeader.js
+++ src/components/TableHeader/TableHeader.js
@@ -1,5 +1,24 @@
+import "./TableHeader.scss";
 const TableHeader = () => {
-  return <div>table header</div>;
+  return (
+    <div className="table-header">
+      <input
+        type="checkbox"
+        className="table-header__checkbox"
+        id=""
+        name=""
+        value=""
+      />
+      <h4 className="table-header__item">1</h4>
+      <h4 className="table-header__item">2</h4>
+      <h4 className="table-header__item">3</h4>
+      <h4 className="table-header__item">4</h4>
+      <h4 className="table-header__item">5</h4>
+      <h4 className="table-header__item">6</h4>
+      <h4 className="table-header__item">7</h4>
+      <h4 className="table-header__item">8</h4>
+    </div>
+  );
 };
 
 export default TableHeader;
